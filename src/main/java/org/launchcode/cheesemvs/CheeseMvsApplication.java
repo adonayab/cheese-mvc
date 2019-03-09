266ee1c28f84302ef2f503bc7a2055f01e06fc11
adonayab <adonayab15@hotmail.com>
2019-03-08 18:47:02 -0600

    Initial Commit

---
+++ src/main/java/org/launchcode/cheesemvs/CheeseMvsApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.cheesemvs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheeseMvsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheeseMvsApplication.class, args);
+	}
+
+}
